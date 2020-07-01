36ecc930ab6ffde681475f4c07602243825122db
Nika Darab <nikadarab@Xaviers-Air.attlocal.net>
2020-07-01 14:16:41 -0500

    update

---
+++ scripts.js
@@ -0,0 +1,6 @@
+ const cards = [
+  obj1={"WATER":"آب"}
+ ]
+let translate= ()=> {
+  let word = document.getElementsByClassName("flip-card-front");
+}
--- style.js
+++
@@ -1,9 +0,0 @@
-let dropDownValue = document.getElementById("dropDown");
-
-dropDownValue.onchange = function() {
-  if (this.selectedIndex !== 0) {
-    window.location.href = this.value;
-    let x = this.value;
-    document.getElementById("dropDown").innerHTML(x);
-  }
-};
